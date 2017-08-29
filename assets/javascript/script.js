6ffb72a987d71bbbc386e0fbbf36c38d8808bd01
Landen Wasserstrom <Landen.Wasserstrom@gmail.com>
2017-08-29 10:58:46 -0700

    formatting

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,6 +5,7 @@
       return false;
   });
 
+$(function() {
 //declair varibles
   let searchButton = $("#search_button")
   let heroName
@@ -16,48 +17,48 @@
   let homeButton = $("#home")
   let orginalURL = "http://gateway.marvel.com/v1/public/characters?limit=100&ts=1&apikey=45ffc7a588682b67fca8b6fd6a866bcb&hash=1b9b9c66cd69a13c4a32ee739962a948"
   let searchURL = orginalURL
-//function to hit the API and grab hero names and pictures then populate thos in a table
-function getHeros() {
-    $.get(searchURL, function (data){
+    //function to hit the API and grab hero names and pictures then populate thos in a table
+  function getHeros() {
+      $.get(searchURL, function (data){
 
-      tableHeading = $("<th>Hero Picture</th><th>Hero Name</th>")
-      $("#heroTable").append(tableHeading)
+        tableHeading = $("<th>Hero Picture</th><th>Hero Name</th>")
+        $("#heroTable").append(tableHeading)
 
-      for (let i = 0; i < data.data.results.length; i++) {
-      heroName = data.data.results[i].name
-      heroIcon = data.data.results[i].thumbnail.path
-      iconExtension = "." + data.data.results[i].thumbnail.extension
-      newRow = $("<tr><td><img src=\"" + heroIcon + iconExtension + "\"/></td>" + "<td><h2>" + heroName + "</h2></td></tr>")
-      $("#heroTable").append(newRow)
-      }
-    })
-  }
+        for (let i = 0; i < data.data.results.length; i++) {
+        heroName = data.data.results[i].name
+        heroIcon = data.data.results[i].thumbnail.path
+        iconExtension = "." + data.data.results[i].thumbnail.extension
+        newRow = $("<tr><td><img src=\"" + heroIcon + iconExtension + "\"/></td>" + "<td><h2>" + heroName + "</h2></td></tr>")
+        $("#heroTable").append(newRow)
+        }
+      })
+    }
 
-//run function
-getHeros()
-
-//rerun getHeros function with new URL based on the search box value
-searchButton.click(function(){
-  $("#heroTable").html("")
-  searchTerm = $("#search").val()
-
-//if search field is blank return orginal get
-  if (searchTerm == "") {
-    searchURL = orginalURL
-    getHeros();
-  }
-  else {
-    searchURL = ("http://gateway.marvel.com/v1/public/characters?limit=100&ts=1&apikey=45ffc7a588682b67fca8b6fd6a866bcb&hash=1b9b9c66cd69a13c4a32ee739962a948&nameStartsWith=" + searchTerm)
-    $("#search").val("")
+    //run function
     getHeros()
-  }
+
+    //rerun getHeros function with new URL based on the search box value
+    searchButton.click(function(){
+      $("#heroTable").html("")
+      searchTerm = $("#search").val()
+
+    //if search field is blank return orginal get
+      if (searchTerm == "") {
+        searchURL = orginalURL
+        getHeros();
+      }
+      else {
+        searchURL = ("http://gateway.marvel.com/v1/public/characters?limit=100&ts=1&apikey=45ffc7a588682b67fca8b6fd6a866bcb&hash=1b9b9c66cd69a13c4a32ee739962a948&nameStartsWith=" + searchTerm)
+        $("#search").val("")
+        getHeros()
+      }
+      })
+
+    //when page header is clicked reload page
+    homeButton.click(function(){
+      $("#heroTable").html("")
+      searchURL = "http://gateway.marvel.com/v1/public/characters?limit=100&ts=1&apikey=45ffc7a588682b67fca8b6fd6a866bcb&hash=1b9b9c66cd69a13c4a32ee739962a948"
+      getHeros()
+    })
   })
-
-//when page header is clicked reload page
-homeButton.click(function(){
-  $("#heroTable").html("")
-  searchURL = "http://gateway.marvel.com/v1/public/characters?limit=100&ts=1&apikey=45ffc7a588682b67fca8b6fd6a866bcb&hash=1b9b9c66cd69a13c4a32ee739962a948"
-  getHeros()
-})
-
 })();
